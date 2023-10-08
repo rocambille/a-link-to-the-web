6cdece34f965eabe7eaf80b72ea066a2a5aa513a
romaing <rocambille@gmail.com>
2023-10-08 11:07:59 +0200

    added Game class and first game lines

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-const username = prompt("Register your name");
+class Game {
+  constructor() {
+    this.username = prompt("Register your name");
 
-alert(`Let's go ${username} ! Ready to play ?`);
+    alert(`Let's go ${this.username} ! Ready to play ?`);
+
+    this.showMessage("Help me...", () => {
+      this.showMessage("Please, help me...");
+    });
+  }
+
+  showMessage(
+    text,
+    next = null,
+    parent = document.querySelector(".typewriter")
+  ) {
+    const p = document.createElement("p");
+
+    p.textContent = text;
+    p.style.setProperty("--animation-duration", `${(text.length * 3.5) / 20}s`);
+
+    (parent ?? document.body).appendChild(p);
+
+    p.addEventListener("animationend", () => {
+      p.style.animation = "initial";
+      p.style.borderRight = "initial";
+
+      if (next != null) {
+        setTimeout(next, 1000);
+      }
+    });
+  }
+}
+
+const game = new Game();
